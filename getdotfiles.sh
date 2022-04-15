51b309744ec7e74cc550a4d7eb7c261e225146b3
Elia <eliiia@protonmail.com>
2022-04-15 19:45:35 +0100

    change getdotfiles script

--- getdotfiles.sh
+++ getdotfiles.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+cd ~/git/dotfiles
+
 # wipe existing data:
-# rm -rf .bashrc .config/
+rm -rf ./.bashrc ./.config/
+
+# mkdir so that cp doesnt fail
+mkdir ./.config
+mkdir ./.config/i3
+mkdir ./.config/i3status
+mkdir ./.config/alacritty
 
 # bashrc
 cp ~/.bashrc ./.bashrc
 
 # i3
-cp -r ~/.config/i3/ ./.config/i3/
-cp -r ~/.config/i3status/ ./.config/i3status/
+cp -r ~/.config/i3/ ./.config/
+cp -r ~/.config/i3status/ ./.config/
 
 # alacritty (terminal)
-cp -r ~/.config/alacritty/ ./.config/alacritty/
+cp -r ~/.config/alacritty/ ./.config/
