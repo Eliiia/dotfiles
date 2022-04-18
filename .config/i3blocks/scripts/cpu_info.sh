11af9ccd44f44772b27b9ffc3cb4ef45cc06bba9
Elia <eliiia@protonmail.com>
2022-04-18 22:54:42 +0100

    cpu_info in i3blocks

---
+++ .config/i3blocks/scripts/cpu_info.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+TEMP=$(sensors | grep 'Tccd1:\|Tdie' | grep '[ ]*+[0-9]*.[0-9]*°C' -o | grep '+[0-9]*.[0-9]*°C' -o)
+CPU_USAGE=$(mpstat 1 1 | awk '/Average:/ {printf("%s\n", $(NF-9))}')
+#echo "$CPU_USAGE ${TEMP:1}" | awk '{ printf("%6s\% / %s\n"), $1, $2 }'
+echo "$CPU_USAGE% ${TEMP:1}" | awk '{ printf("%6s %6s\n"), $1, $2 }'
