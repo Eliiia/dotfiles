24a105405a3e474c081221dd4f8b2723b787ad54
Elia <eliiia@protonmail.com>
2022-04-27 21:32:01 +0100

    change something in cpu_info script

--- .config/i3blocks/scripts/cpu_info.sh
+++ .config/i3blocks/scripts/cpu_info.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-TEMP=$(sensors | grep 'Tccd1:\|Tdie' | grep '[ ]*+[0-9]*.[0-9]*°C' -o | grep '+[0-9]*.[0-9]*°C' -o)
+#TEMP=$(sensors | grep 'Tccd1:\|Tdie' | grep '[ ]*+[0-9]*.[0-9]*°C' -o | grep '+[0-9]*.[0-9]*°C' -o)
 CPU_USAGE=$(mpstat 1 1 | awk '/Average:/ {printf("%s\n", $(NF-9))}')
 #echo "$CPU_USAGE ${TEMP:1}" | awk '{ printf("%6s\% / %s\n"), $1, $2 }'
-echo "$CPU_USAGE% ${TEMP:1}" | awk '{ printf("%6s %6s\n"), $1, $2 }'
+#echo "$CPU_USAGE% ${TEMP:1}" | awk '{ printf("%6s %6s\n"), $1, $2 }' 
+echo "$CPU_USAGE%" | awk '{ printf(" %s%\n"), int($1) }'
